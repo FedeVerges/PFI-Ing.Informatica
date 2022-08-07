97dede52a6612c646350aea581a315ee1251c327
Fede <fedeverges@gmail.com>
2022-08-07 20:41:54 -0300

    Se agrega dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import { App } from "./app";
 
 
 async function initServer() {
-    const app = new App(8080);
+    const app = new App(process.env.PORT);
     await app.listen();
 }
 initServer();
