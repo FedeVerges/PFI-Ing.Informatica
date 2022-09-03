37fc725f3681fd0c72d3ed20d96910f8a8567ec5
Fede <fedeverges@gmail.com>
2022-09-03 00:23:41 -0300

    Se agrega metodo asincrono para publicar transaccion

--- src/services/certificates/certificatesService.ts
+++ src/services/certificates/certificatesService.ts
@@ -1,9 +1,10 @@
-import { CertificateDto } from "../../dto/certificateDto";
-import { TransactionDto } from "../../dto/transactionDto";
-import { web3Service } from "../../services/web3/web3Service";
-import { CertificateEth, fromDto } from "../../models/blockchain/certificateEth";
-import { Certificate } from "../../models/certificate";
-import { BlockchainTransaction } from "../../models/transaction";
+import {CertificateDto} from "../../dto/certificateDto";
+import {TransactionDto} from "../../dto/transactionDto";
+import {web3Service} from "../../services/web3/web3Service";
+import {CertificateEth, fromDto} from "../../models/blockchain/certificateEth";
+import {Certificate} from "../../models/certificate";
+import {BlockchainTransaction} from "../../models/transaction";
+import {TransactionReceipt} from "web3-core";
 
 export const CertificateService = {
     async getCertificatesByStudentId(id: number) {
@@ -72,40 +73,13 @@ export const CertificateService = {
             const transactionResponse = await transaction.save();
 
             // Envio a publicar la transaccion.
-            const [resultCertificate, receipt] = await web3Service.sendTransaction(signed);
+            // Mandar a publicar la trnasaccion de manera asincrona.
+            web3Service.sendTransaction(signed)
+                .then(
+                    async ([resultCertificate, receipt]) =>
+                        this.updateStateTransaction(transactionResponse, resultCertificate, receipt));
 
-            transactionResponse.set({
-                status: 'COMPLETED',
-                ceritificateBlockchainId: resultCertificate?.id,
-                blockNumber: receipt.blockNumber,
-                blockHash: receipt.blockHash,
-                from: receipt.from,
-                gasUsed: receipt.gasUsed,
-            });
-
-            await transactionResponse.save();
-
-            // Con el resultado de la transaccion, actualizamos la transaccion y el certificado.
-
-
-
-
-
-
-            // const [newCertificate, receipt] = await web3Service.createCertificate(certificateData);
-
-            // Validamos el retorno de la misma corroborando en errores.
-
-
-            // Obtenemos datos de la blockchain como numero de bloque, num de transaccion, hashes, costo de gas de la transacción y etc que hagan falta.
-            // Guardamos esa informacion en la base. Entidad TRANSACITON.
-
-
-            // Hacer el backup en la bd local.
-            // verificar con dni o identificador particular. 
-            // Quien es el estudiante? Si existe en el sistema, creamos la relacion y el certificado.
-            // Si no existe en el sistema, se crea la persona y por lo tanto, el estudiante con los datos del certificado unicamente.
-
+            //Todo: Mientras tranto, se informa al usuario la publicacion de la transaccion y el estado (Pendiente).
             const transactionRes = {
                 receipt: transactionResponse,
                 certificate: certificateData
@@ -114,6 +88,22 @@ export const CertificateService = {
         } catch (error) {
             throw error;
         }
+    },
+
+    async updateStateTransaction(transactionResponse: BlockchainTransaction, resultCertificate: Partial<CertificateEth> | null, receipt: TransactionReceipt) {
+        // Con el resultado de la transaccion, actualizamos la transaccion y el certificado.
+        transactionResponse.set({
+            status: 'COMPLETED',
+            ceritificateBlockchainId: resultCertificate?.id,
+            blockNumber: receipt.blockNumber,
+            blockHash: receipt.blockHash,
+            from: receipt.from,
+            gasUsed: receipt.gasUsed,
+        });
+        await transactionResponse.save();
+
+        // Validamos el retorno de la misma corroborando en errores.
+
     },
 
     async deleteCertificate(id: number) {
@@ -126,30 +116,30 @@ export const CertificateService = {
 
 }
 
-   // const student = new Student(
-            //     {
-            //         name: certificateData.student.name,
-            //         lastName: certificateData.student.lastname,
-            //         fullName: `${certificateData.student.name} ${certificateData.student.lastname}`,
-            //         docNumber: certificateData.student.docNumber,
-            //     } as Student
-            // )
-            // // Guarda estudiante.
+// const student = new Student(
+//     {
+//         name: certificateData.student.name,
+//         lastName: certificateData.student.lastname,
+//         fullName: `${certificateData.student.name} ${certificateData.student.lastname}`,
+//         docNumber: certificateData.student.docNumber,
+//     } as Student
+// )
+// // Guarda estudiante.
 
-            // // Crea certificado.
-            // const certificate = new Certificate(
-            //     {
-            //         name: certificateData.degreeName,
-            //         institutionId: certificateData.institutionId,
-            //         degreeName: certificateData.degreeName,
-            //         dateCreated: new Date().toISOString(),
-            //         dateModified: new Date().toISOString(),
-            //         waferNumber: certificateData.waferNumber,
-            //         volumeNumber: certificateData.volumeNumber,
-            //         recordNumber: certificateData.recordNumber,
-            //         studentId: student.id,
-            //     } as Certificate
-            // )
-            // TODO: agregar nombre de Facultad y lookup.
-            // Nombre de plan y id.
-            // Ver como validar el acta y el tomo.
+// // Crea certificado.
+// const certificate = new Certificate(
+//     {
+//         name: certificateData.degreeName,
+//         institutionId: certificateData.institutionId,
+//         degreeName: certificateData.degreeName,
+//         dateCreated: new Date().toISOString(),
+//         dateModified: new Date().toISOString(),
+//         waferNumber: certificateData.waferNumber,
+//         volumeNumber: certificateData.volumeNumber,
+//         recordNumber: certificateData.recordNumber,
+//         studentId: student.id,
+//     } as Certificate
+// )
+// TODO: agregar nombre de Facultad y lookup.
+// Nombre de plan y id.
+// Ver como validar el acta y el tomo.
